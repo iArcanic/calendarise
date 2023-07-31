33c1d86323362bd42a3dfdb7a43dd53876d0cfe2
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-07-31 20:29:02 +0100

    Added React routing dependency and created initial UI skeleton components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'; // Update the import statement here
+import MainPage from './MainPage';
+import IndividualCalendar from './IndividualCalendar';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes> {/* Update Switch to Routes */}
+          <Route path="/" element={<MainPage />} />
+          <Route path="/calendars/:id" element={<IndividualCalendar />} />
+        </Routes> {/* Update Route to use "element" prop */}
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/MainPage.js
@@ -0,0 +1,27 @@
+// src/MainPage.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const MainPage = () => {
+  // Mock data for user calendars
+  const userCalendars = [
+    { id: '1', name: 'Family Events', description: 'Family gatherings and events' },
+    { id: '2', name: 'Work Schedule', description: 'Work-related events and meetings' },
+    // Add more mock calendars as needed
+  ];
+
+  return (
+    <div>
+      <h1>User Calendars</h1>
+      {userCalendars.map((calendar) => (
+        <div key={calendar.id}>
+          <h3>{calendar.name}</h3>
+          <p>{calendar.description}</p>
+          <Link to={`/calendars/${calendar.id}`}>View Calendar</Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default MainPage;
