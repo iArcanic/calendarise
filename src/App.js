b2922c28e3cfa626f293178471a71e9b874a1aff
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-08-02 11:37:55 +0100

    Rearranged UI components, i.e. FullCalendar, to be suitable for easy app navigation.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'; // Update the import statement here
+
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
 import MainPage from './MainPage';
-import IndividualCalendar from './IndividualCalendar';
 import CreateCalendar from './CreateCalendar';
-import Calendar from './Calendar';
+import FullCalendarComponent from './FullCalendar';
 
 const App = () => {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes> {/* Update Switch to Routes */}
+        <Routes>
           <Route path="/" element={<MainPage />} />
-          <Route path="/calendars/:id" element={<IndividualCalendar />} />
 	  <Route path="/new-calendar" element={<CreateCalendar />} />
-	  <Route path="/calendar" element={<Calendar />} /> {/* Add the route */}
-        </Routes> {/* Update Route to use "element" prop */}
+	  <Route path="/calendars/:id" element={<FullCalendarComponent />} />
+        </Routes>
       </div>
     </Router>
   );
--- src/MainPage.js
+++ src/MainPage.js
@@ -1,18 +1,35 @@
 // src/MainPage.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import FullCalendarComponent from './FullCalendar'
 
 const MainPage = () => {
   // Mock data for user calendars
   const userCalendars = [
-    { id: '1', name: 'Family Events', description: 'Family gatherings and events' },
-    { id: '2', name: 'Work Schedule', description: 'Work-related events and meetings' },
-    // Add more mock calendars as needed
+    { 
+	    id: '1',
+	    name: 'Family Events',
+	    description: 'Family gatherings and events',
+	    events: [
+		{ title: 'Family Reuinion', date: '2023-08-10' },
+		{ title: 'Birthday Party', date: '2023-08-15' }
+	    ]
+    },
+    { 
+	    id: '2',
+	    name: 'Work Schedule',
+	    description: 'Work-related events and meetings',
+	    events: [
+		{ title: 'Team Meeting', date: '2023-08-12' },
+		{ title: 'Project Deadline', date: '2023-08-15' }
+	    ]
+    },
   ];
 
   return (
     <div>
       <h1>User Calendars</h1>
+      <Link to="/new-calendar">Create New Calendar</Link>
       {userCalendars.map((calendar) => (
         <div key={calendar.id}>
           <h3>{calendar.name}</h3>
@@ -20,8 +37,6 @@ const MainPage = () => {
           <Link to={`/calendars/${calendar.id}`}>View Calendar</Link>
         </div>
       ))}
-	  <Link to="/new-calendar">Create New Calendar</Link>
-	  <Link to="/calendar">View Calendar</Link>
     </div>
   );
 };
